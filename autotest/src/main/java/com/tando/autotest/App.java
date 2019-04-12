6ed3ea937957ad15bf61824bf65cbe1c0dacb2f4
tando <tando@qq.com>
2019-04-12 17:15:09 +0800

    Added two drivers and update pom.xml.

--- autotest/src/main/java/com/tando/autotest/App.java
+++ autotest/src/main/java/com/tando/autotest/App.java
@@ -1,13 +1,27 @@
 package com.tando.autotest;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws InterruptedException
     {
         System.out.println( "Hello World!" );
+        System.setProperty("webdriver.ie.driver", ".\\Tools\\IEDriverServer.exe");
+        WebDriver driver = new InternetExplorerDriver();
+        driver.manage().window().maximize();
+        driver.get("http://www.baidu.com");
+        String s = driver.getTitle();
+        System.out.print(s);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Thread.sleep(1000);
+        driver.close();
     }
 }
