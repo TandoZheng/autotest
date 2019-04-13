a0e63abff181853012ecd5bd6bdd6c8998597664
duanjie <duanjie@LAPTOP-AUBF06OK.lan>
2019-04-13 11:06:13 +0800

    commit new

--- autotest/src/main/java/com/tando/autotest/core/UI.java
+++ autotest/src/main/java/com/tando/autotest/core/UI.java
@@ -51,22 +51,52 @@ public class UI {
 			return null;
 		}
 	}
-
+	
+	//close
+	public void close() {
+		driver.close();
+	}
+	
+	//quit
+	public void quit() {
+		driver.quit();
+	}
+	
 	//clear value
 	public void clear(By by) {
 		element(by).clear();
 	}
 	
-	//get attribute
-	public String getAttribute(By by,String name) {
-		if(isExisted(by)) {
-			return element(by).getAttribute(name);
-		}else {
-			return null;
-		}
-		
+	//get url
+	public String getCurrentUrl(){
+		return driver.getCurrentUrl();
 	}
 	
+	//verify tag name successful
+	public void verifyCurrentUrlOk(String urlExpected) {
+		Assert.assertEquals("verify url successful!" + urlExpected, getCurrentUrl());
+	}
+		
+	//verify tag name failed
+	public void verifyCurrentUrlFailed(By by,String urlUnexpected) {
+		Assert.assertNotEquals("verify url failed!" + urlUnexpected, getTagName(by));
+	}
+	
+	//get attribute
+	public String getAttribute(By by, String attributeName) {
+		return element(by).getAttribute(attributeName);
+	}
+	
+	//verify attribute successful
+	public void verifyAttributeOk(By by,String attributeName,String attributeExpected) {
+		Assert.assertEquals(attributeExpected, getAttribute(by, attributeName));
+	}
+	
+	//verify attribute failed
+	public void verifyAttributeFailed(By by,String attributeName,String attributeUnexpected) {
+		Assert.assertNotEquals(attributeUnexpected, getAttribute(by, attributeName));
+	}
+		
 	//equals
 	private boolean equals(By by,Object obj) {
 		if(isExisted(by)) {
@@ -74,31 +104,36 @@ public class UI {
 		}else {
 			return false;
 		}
-		
-	}
-	
-	//verify value is equals the value we wanted
-	private void assertEquals(By by,Object expected,Object actual) {
-		Assert.assertEquals(expected, actual);
 	}
 	
 	//get tag name
 	public String getTagName(By by) {
-		if(isExisted(by)) {
-			return element(by).getTagName();
-		}else {
-			return null;
-		}
-		
+		return element(by).getTagName();
+	}
+	
+	//verify tag name successful
+	public void verifyTagNameOk(By by,String tagNameExpected) {
+		Assert.assertEquals("verify tag name successful!" + tagNameExpected, getTagName(by));
+	}
+	
+	//verify tag name failed
+	public void verifyTagNameFailed(By by,String tagNameUnexpected) {
+		Assert.assertNotEquals("verify tag name failed!" + tagNameUnexpected, getTagName(by));
 	}
 	
 	// get text
 	public String getText(By by) {
-		if(isExisted(by)) {
-			return element(by).getText();
-		}else {
-			return null;
-		}
+		return element(by).getText();
+	}
+	
+	//verify text successful
+	public void verifyTextOk(By by,String textExpected) {
+		Assert.assertEquals("verify text successful!" + textExpected, getText(by));
+	}
+		
+	//verify text failed
+	public void verifyTextFailed(By by,String textUnexpected) {
+		Assert.assertNotEquals("verify text failed!" + textUnexpected, getText(by));
 	}
 	
 	//get size
@@ -111,10 +146,18 @@ public class UI {
 		return driver.getTitle();
 	}
 	
+	//verify title successful
+	public void verifyTitleOk(String titleExpected) {
+		Assert.assertEquals("verify title successful!" + titleExpected, getTitle());
+	}
+			
+	//verify title failed
+	public void verifyTitleFailed(String titleUnexpected) {
+		Assert.assertNotEquals("verify title failed!" + titleUnexpected, getTitle());
+	}
 		
 	// verify element is existed
 	private boolean isExisted(By by) {
-
 		try {
 			driver.findElement(by);
 			return true;
@@ -125,7 +168,6 @@ public class UI {
 
 	// verify element is displayed
 	private boolean isDisplayed(By by) {
-
 		if (isExisted(by)) {
 			return element(by).isDisplayed();
 		} else {
@@ -153,35 +195,44 @@ public class UI {
 	}
 	
 	// verify element is existed
-		public void verifyExisted(By by) {
+	public void verifyExisted(By by) {
+		Assert.assertTrue("Element verify exist successful!" + by.toString(), isExisted(by));
+	}
 
-			Assert.assertTrue("Element verify exist successful!" + by.toString(), isExisted(by));
-		}
-
-		// verify element is not existed
-		public void verifyNotExisted(By by) {
-
-			Assert.assertFalse("Element verify not exist failed!" + by.toString(), isExisted(by));
-		}
+	// verify element is not existed
+	public void verifyNotExisted(By by) {
+		Assert.assertFalse("Element verify not exist failed!" + by.toString(), isExisted(by));
+	}
 		
-		// verify element is displayed
-		public void verifyDisplayed(By by) {
-			Assert.assertTrue("Element verify display successful!" + by.toString(), isDisplayed(by));
-		}
-		// verify element is not displayed
-		public void verifyNotDisplayed(By by) {
-			Assert.assertFalse("Element verify display failed!" + by.toString(), isDisplayed(by));
-		}
+	// verify element is displayed
+	public void verifyDisplayed(By by) {
+		Assert.assertTrue("Element verify display successful!" + by.toString(), isDisplayed(by));
+	}
 		
-		// verify element is selected
-		public void verifySelected(By by) {
-			Assert.assertTrue("Element verify select successful!" + by.toString(), isSelected(by));
-		}
+	// verify element is not displayed
+	public void verifyNotDisplayed(By by) {
+		Assert.assertFalse("Element verify display failed!" + by.toString(), isDisplayed(by));
+	}
 		
-		// verify element is not selected
-		public void verifyNotSelected(By by) {
-			Assert.assertFalse("Element verify select failed!" + by.toString(), isSelected(by));
-		}
+	// verify element is selected
+	public void verifySelected(By by) {
+		Assert.assertTrue("Element verify select successful!" + by.toString(), isSelected(by));
+	}
+		
+	// verify element is not selected
+	public void verifyNotSelected(By by) {
+		Assert.assertFalse("Element verify select failed!" + by.toString(), isSelected(by));
+	}
+		
+	// verify element is enabled
+	public void verifyEnabled(By by) {
+		Assert.assertTrue("Element verify select successful!" + by.toString(), isEnabled(by));
+	}
+				
+	// verify element is disabled
+	public void verifyDisabled(By by) {
+		Assert.assertFalse("Element verify select failed!" + by.toString(), isEnabled(by));
+	}
 		
 	// wait time
 	public void waitTime() {
